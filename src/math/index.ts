362b0c6f1f1ec330659c15c5dcd9b5ec2548d22e
Slava Olenin <slava.olenin@smartnews.com>
2022-12-04 17:17:03 +0900

    chore: export constant math types

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,10 +1,10 @@
 import { Color, ColorValue } from 'flexidy-engine-base/core/math/color';
-import { Vector2 } from 'flexidy-engine-base/core/math/vector2';
-import { Vector3 } from 'flexidy-engine-base/core/math/vector3';
-import { Vector4 } from 'flexidy-engine-base/core/math/vector4';
-import { Quaternion } from 'flexidy-engine-base/core/math/quaternion';
-import { Matrix3 } from 'flexidy-engine-base/core/math/matrix3';
-import { Matrix4 } from 'flexidy-engine-base/core/math/matrix4';
+import { CVector2, Vector2 } from 'flexidy-engine-base/core/math/vector2';
+import { CVector3, Vector3 } from 'flexidy-engine-base/core/math/vector3';
+import { CVector4, Vector4 } from 'flexidy-engine-base/core/math/vector4';
+import { CQuaternion, Quaternion } from 'flexidy-engine-base/core/math/quaternion';
+import { CMatrix3, Matrix3 } from 'flexidy-engine-base/core/math/matrix3';
+import { CMatrix4, Matrix4 } from 'flexidy-engine-base/core/math/matrix4';
 import { Spherical } from 'flexidy-engine-base/core/math/spherical';
 // import { DEG2RAD, RAD2DEG, ceil, clamp, degToRad, floor, generateUUID, radToDeg } from 'flexidy-engine-base/math/math';
 
@@ -29,4 +29,13 @@ export {
   Matrix3,
   Matrix4,
   Spherical,
+};
+
+export type {
+  CVector2,
+  CVector3,
+  CVector4,
+  CQuaternion,
+  CMatrix3,
+  CMatrix4,
 };
